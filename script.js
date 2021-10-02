bc38cbe578e687a54121d366b31529dda94926c2
mjhwest89 <mjhwest89@gmail.com>
2021-10-02 14:39:39 +0930

    trying different hour options to see what happens

--- script.js
+++ script.js
@@ -3,3 +3,21 @@
 //setting the variable currentDate, 
 var currentDate = moment().format('dddd  MMMM Do, YYYY');
 $("#currentDay").html(currentDate);
+
+// var CurrentDate = moment().time(); //dosent work look at moment website and find how to display hour
+// console.log(currentDate)
+
+// var timeNow = moment().hours();
+// console.log(timeNow)
+
+var timeNow = moment().hour(Number);
+console.log(timeNow)
+
+var timeNow = moment().hour(); // Number
+console.log(timeNow)
+
+var timeNow = moment().hours(Number);
+console.log(timeNow)
+
+var timeNow = moment().hours(); // Number
+console.log(timeNow)
